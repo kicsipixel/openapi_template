3b1fd095cb120e1261cde633c57263c7ac35bcaf
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-12-21 18:00:01 +0100

    README.md was updated with the correct link.

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -5,28 +5,28 @@ import OpenAPIRuntime
 
 @main
 struct HummingbirdAPIService: AsyncParsableCommand {
-    @Option(name: .shortAndLong)
-    var hostname = "127.0.0.1"
-    
-    @Option(name: .shortAndLong)
-    var port = 8080
-    
-    func run() async throws {
-        let router = Router()
-        router.middlewares.add(LogRequestsMiddleware(.info))
-        
-        let api = APIServiceImpl()
-        try api.registerHandlers(on: router)
-        
-        let app = Application(
-            router: router,
-            configuration: .init(address: .hostname(hostname, port: port))
-        )
-        
-        try await withThrowingDiscardingTaskGroup { group in
-            group.addTask {
-                try await app.runService()
-            }
-        }
+  @Option(name: .shortAndLong)
+  var hostname = "127.0.0.1"
+
+  @Option(name: .shortAndLong)
+  var port = 8080
+
+  func run() async throws {
+    let router = Router()
+    router.middlewares.add(LogRequestsMiddleware(.info))
+
+    let api = APIServiceImpl()
+    try api.registerHandlers(on: router)
+
+    let app = Application(
+      router: router,
+      configuration: .init(address: .hostname(hostname, port: port))
+    )
+
+    try await withThrowingDiscardingTaskGroup { group in
+      group.addTask {
+        try await app.runService()
+      }
     }
+  }
 }
